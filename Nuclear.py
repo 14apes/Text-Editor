d11a4c84f97e132caa28596ae6dc1dbd39533433
14apes <17MI554@nith.ac.in>
2020-01-04 18:33:23 +0530

    .

--- Nuclear.py
+++ Nuclear.py
@@ -16,7 +16,34 @@ main_menu = tk.Menu()
 #exit = tk.PhotoImage(load = "exit.png")
 
 file = tk.Menu(main_menu,tearoff =False)
+edit = tk.Menu(main_menu,tearoff = False)
+view = tk.Menu(main_menu,tearoff =False)
+theme= tk.Menu(main_menu,tearoff = False)
+
 main_menu.add_cascade(label = "File", menu = file)
+main_menu.add_cascade(label = "Edit", menu = edit)
+main_menu.add_cascade(label = "View", menu = view)
+main_menu.add_cascade(label = "Theme", menu = theme)
+
+view.add_command(label = "Copy",compound = tk.LEFT,accelerator = "ctrl+C")
+view.add_command(label = "Cut",compound = tk.LEFT,accelerator = "ctrl+X")
+view.add_command(label = "Paste",compound = tk.LEFT,accelerator = "ctrl+V")
+view.add_command(label = "Replace",compound = tk.LEFT,accelerator = "ctrl+H")
+view.add_command(label = "Find",compound = tk.LEFT,accelerator = "ctrl+F")
+
+
+file.add_command(label = "New",compound = tk.LEFT,accelerator = "ctrl+N")
+file.add_command(label = "Open",compound = tk.LEFT,accelerator = "ctrl+O")
+file.add_command(label = "Save",compound = tk.LEFT,accelerator = "ctrl+S")
+file.add_command(label = "Save As",compound = tk.LEFT,accelerator = "ctrl+shift+s")
+file.add_command(label = "Exit",compound = tk.LEFT,accelerator = "ctrl+Q")
+
+edit.add_command(label = "Copy",compound = tk.LEFT,accelerator = "ctrl+C")
+edit.add_command(label = "Cut",compound = tk.LEFT,accelerator = "ctrl+X")
+edit.add_command(label = "Paste",compound = tk.LEFT,accelerator = "ctrl+V")
+edit.add_command(label = "Replace",compound = tk.LEFT,accelerator = "ctrl+H")
+edit.add_command(label = "Find",compound = tk.LEFT,accelerator = "ctrl+F")
+
 
 file.add_command(label = "New",compound = tk.LEFT,accelerator = "ctrl+N")
 file.add_command(label = "Open",compound = tk.LEFT,accelerator = "ctrl+O")
