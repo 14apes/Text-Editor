ab1b51fffd847535c992ce40d858870473e6fb90
14apes <17MI554@nith.ac.in>
2020-01-04 13:48:24 +0530

    .

--- Nuclear.py
+++ Nuclear.py
@@ -7,4 +7,25 @@ nuclear=tk.Tk()
 nuclear.geometry("500x400")
 nuclear.title("nuclear")  
 
+main_menu = tk.Menu()
+
+#new_icon = tk.PhotoImage(file = "new.png")
+#openn = tk.PhotoImage(load = "")
+#save = tk.PhotoImage(load = "save.png")
+#save_as = tk.PhotoImage(load = "")
+#exit = tk.PhotoImage(load = "exit.png")
+
+file = tk.Menu(main_menu,tearoff =False)
+main_menu.add_cascade(label = "File", menu = file)
+
+file.add_command(label = "New",compound = tk.LEFT,accelerator = "ctrl+N")
+file.add_command(label = "Open",compound = tk.LEFT,accelerator = "ctrl+O")
+file.add_command(label = "Save",compound = tk.LEFT,accelerator = "ctrl+S")
+file.add_command(label = "Save As",compound = tk.LEFT,accelerator = "ctrl+shift+s")
+file.add_command(label = "Exit",compound = tk.LEFT,accelerator = "ctrl+Q")
+
+
+
+nuclear.config(menu = main_menu)
+ 
 nuclear.mainloop()
