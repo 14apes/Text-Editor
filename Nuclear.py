df3fff378e54a61c4d975e5ad0aedb1492cbefae
14apes <17MI554@nith.ac.in>
2020-01-04 13:01:07 +0530

    .

---
+++ Nuclear.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font,colorchooser,filedialog,messagebox
+import os
+
+nuclear=tk.Tk()
+nuclear.geometry("500x400")
+nuclear.title("nuclear")  
+
+nuclear.mainloop()
